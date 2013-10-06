c276012a40120345de5f24e92be5ce19308cb325
Tomasz Cudok <tcudok@gmail.com>
2013-10-06 18:40:09 +0200

    Code cleanup in Point3 class.

---
+++ XnaCraft/Engine/Vector3Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace XnaCraft.Engine
+{
+    public static class Vector3Extensions
+    {
+        public static Point3 ToPoint3(this Vector3 vector)
+        {
+            return new Point3((int)Math.Round(vector.X), (int)Math.Round(vector.Y), (int)Math.Round(vector.Z));
+        }
+
+        public static Vector3 ToVector3(this Point3 point)
+        {
+            return new Vector3(point.X, point.Y, point.Z);
+        }
+    }
+}
